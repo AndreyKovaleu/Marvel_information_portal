dcb2bdd29f581331997c5a5e635a0fed31688765
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-01-19 17:34:22 +0300

    added download counter and error message

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -29,7 +29,7 @@ class MarvelService {
   _transformCharacter = (char) => {
     return {
       name: char.name,
-      description: char.description,
+      description: char.description ? `${char.description.slice(0, 210)}...` : "Character description not found",
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url
