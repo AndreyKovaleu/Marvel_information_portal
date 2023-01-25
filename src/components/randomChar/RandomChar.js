415791354c8803e79885321ca1370e319aff76d9
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-01-25 20:19:03 +0300

    added "objectfit" if image not found

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,3 @@
-// import { Component } from 'react/cjs/react.production.min'; // ? Что это ///
-// import { Component } from 'react/cjs/react.production.min';
 import { Component } from 'react';
 import Spinner from '../spiner/Spiner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -75,9 +73,14 @@ class RandomChar extends Component {
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki } = char;
 
+    let imgStyle = { objectFit: 'cover' };
+    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+        imgStyle = { objectFit: "contain" };
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle} />
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
