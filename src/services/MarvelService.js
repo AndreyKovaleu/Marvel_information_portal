85d706d1070bc2d1b7fbe91384bbf15bf05ea82d
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-04 15:31:10 +0300

    added download information about the character from the server

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -31,7 +31,9 @@ class MarvelService {
       description: char.description ? `${char.description.slice(0, 210)}...` : "Character description not found",
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
-      wiki: char.urls[1].url
+      wiki: char.urls[1].url,
+      // comics: char.data.results[0].comics.items
+      comics: char.comics.items
     }
   }
 }
